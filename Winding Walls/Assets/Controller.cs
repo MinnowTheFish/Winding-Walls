072572c339680a2fddc014426f3c5cb1c0048a03
Daniel <danieleded2@gmail.com>
2022-05-08 20:43:47 +1200

    Added Gravity, added more to basic scen
    + Added some gravity tests to the main scene
    + Added gravity to the character controller
    + Added comments to Controller.cs
    + Made the materials not shiny and reflective

--- Winding Walls/Assets/Controller.cs
+++ Winding Walls/Assets/Controller.cs
@@ -6,8 +6,10 @@ public class Controller : MonoBehaviour
 {
     public CharacterController _CharacterController;
     public Camera Cam;
-    public float speed = 6;
+    public float speed = 6, gravityScale = 1;
     public Vector2 CamLimit, MouseSensitivity;
+
+    private float VerticalVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +20,25 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
-        _CharacterController.Move(speed * Time.deltaTime * move);
+        // Movement
+        Vector3 move = transform.forward * Input.GetAxis("Vertical") // Backwards/Forwards movement
+                     + transform.right * Input.GetAxis("Horizontal"); // Left/Right movement
+        _CharacterController.Move(speed * Time.deltaTime * move); // Move the CharacterController by the product of move, change in time, and the character speed
 
-        transform.Rotate(0, Input.GetAxis("Mouse X") * MouseSensitivity.x, 0);
-        Cam.transform.Rotate(-Input.GetAxis("Mouse Y") * MouseSensitivity.y, 0, 0);
+        // Camera Rotation
+        transform.Rotate(0, Input.GetAxis("Mouse X") * MouseSensitivity.x, 0); // Rotate the camera horizontally
+        Cam.transform.Rotate(-Input.GetAxis("Mouse Y") * MouseSensitivity.y, 0, 0); // Rotate the camera vertically
 
+        // Limiting the vertical camera rotation
         Vector3 currentRotation = Cam.transform.localEulerAngles;
-        if (currentRotation.x > 180)
+        if (currentRotation.x > 180) // limit the period of the rotation, -180 < θ < 180, rather than 0 < θ < 360
             currentRotation.x -= 360;
-        currentRotation.x = Mathf.Clamp(currentRotation.x, CamLimit.x, CamLimit.y);
-        Cam.transform.localRotation = Quaternion.Euler(currentRotation);
+        currentRotation.x = Mathf.Clamp(currentRotation.x, CamLimit.x, CamLimit.y); // clamp the rotation
+        Cam.transform.localRotation = Quaternion.Euler(currentRotation); // change the camera rotation
+
+        // Apply gravity
+        if (_CharacterController.isGrounded) VerticalVelocity = 0; // if charcter is touching the ground, set vertical velocity
+        else VerticalVelocity -= Time.deltaTime * 9.807f; // else subtract gravity from the character
+        _CharacterController.Move(Vector3.up * VerticalVelocity * Time.deltaTime); // apply the velocity
     }
 }
