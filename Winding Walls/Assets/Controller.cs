5c9f9a79a7309a603ed6ff1576dd323ca019da3b
Daniel <danieleded2@gmail.com>
2022-05-08 23:45:51 +1200

    Death, Input, Flashlight, and More!
    + Added Death(); function
       + Resets character to initial position
       + Resets camera angle
       + Resets Flashlight status
    + Added flashlight and bodylight
       - Player starts with flashlight off and body light on
    + Added basic first room
    + Changed .gitignore to include .blend[0-9] files
    + Added Tiny Bricks material

--- Winding Walls/Assets/Controller.cs
+++ Winding Walls/Assets/Controller.cs
@@ -6,6 +6,16 @@ public class Controller : MonoBehaviour
 {
     public CharacterController _CharacterController;
     public Camera Cam;
+    public Light FlashLight, BodyLight;
+    public bool FlashLightOn {
+        get { return _FlashLightOn; }
+        set {
+            _FlashLightOn = value;
+            FlashLight.gameObject.SetActive(value);
+            BodyLight.gameObject.SetActive(!value);
+        }
+    }
+    private bool _FlashLightOn = false;
     public float speed = 6, gravityScale = 1;
     public Vector2 CamLimit, MouseSensitivity;
 
@@ -14,20 +24,23 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Toggle Flashlight")) 
+            FlashLightOn = !FlashLightOn;
+
         // Movement
-        Vector3 move = transform.forward * Input.GetAxis("Vertical") // Backwards/Forwards movement
-                     + transform.right * Input.GetAxis("Horizontal"); // Left/Right movement
+        Vector3 move = transform.forward * Mathf.Clamp(Input.GetAxis("Joystick Forwards") + Input.GetAxis("Forwards"), -1, 1) // Backwards/Forwards movement
+                     + transform.right * Mathf.Clamp(Input.GetAxis("Joystick Sideways") + Input.GetAxis("Sideways"), -1, 1); // Left/Right movement
         _CharacterController.Move(speed * Time.deltaTime * move); // Move the CharacterController by the product of move, change in time, and the character speed
 
         // Camera Rotation
-        transform.Rotate(0, Input.GetAxis("Mouse X") * MouseSensitivity.x, 0); // Rotate the camera horizontally
-        Cam.transform.Rotate(-Input.GetAxis("Mouse Y") * MouseSensitivity.y, 0, 0); // Rotate the camera vertically
+        transform.Rotate(0, Mathf.Clamp(Input.GetAxis("Mouse X") + Input.GetAxis("Joystick Look X"), -1, 1) * MouseSensitivity.x, 0); // Rotate the camera horizontally
+        Cam.transform.Rotate(-Mathf.Clamp(Input.GetAxis("Mouse Y") + Input.GetAxis("Joystick Look Y"), -1, 1) * MouseSensitivity.y, 0, 0); // Rotate the camera vertically
 
         // Limiting the vertical camera rotation
         Vector3 currentRotation = Cam.transform.localEulerAngles;
@@ -40,5 +53,19 @@ public class Controller : MonoBehaviour
         if (_CharacterController.isGrounded) VerticalVelocity = 0; // if charcter is touching the ground, set vertical velocity
         else VerticalVelocity -= Time.deltaTime * 9.807f; // else subtract gravity from the character
         _CharacterController.Move(Vector3.up * VerticalVelocity * Time.deltaTime); // apply the velocity
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Hit!");
+        if (other.tag == "Respawn")
+            Death();
+    }
+
+    public void Death() {
+        _CharacterController.transform.position = new Vector3(0, 0, 11.5f);
+        VerticalVelocity = 0;
+        _CharacterController.transform.rotation = Quaternion.Euler(Vector3.zero);
+        Cam.transform.rotation = Quaternion.Euler(Vector3.zero);
+        FlashLightOn = false;
     }
 }
